07355fb3c4b5cc1f707778f3c8f1a4ba812f1ce3
Andrew Obwocha <zeros-faddish.0t@icloud.com>
2026-03-03 19:29:53 -0700

    chore: update resume template

--- src/templates/templates.py
+++ src/templates/templates.py
@@ -1,12 +1,20 @@
 RESUME_LATEX = r"""
-\documentclass[11pt]{article}
+\documentclass[10pt]{extarticle}
 \usepackage[margin=0.5in]{geometry}
 \usepackage{enumitem}
-\usepackage[colorlinks=true, urlcolor=blue]{hyperref}
+\usepackage[colorlinks=true, urlcolor=black]{hyperref}
 \usepackage{titlesec}
+\usepackage[T1]{fontenc}     
+\usepackage{newtxtext}
+\usepackage{anyfontsize}
+
+\pagestyle{empty}
+
+\linespread{0.95} 
 
 % Variables
 \newcommand{\Name}{Andrew Obwocha}
+\newcommand{\City}{Edmonton, AB}
 \newcommand{\Email}{aobwocha@ualberta.ca}
 \newcommand{\Phone}{(780)-717-0986}
 \newcommand{\Github}{https://github.com/AndrewObwocha}
@@ -14,99 +22,112 @@ RESUME_LATEX = r"""
 
 % Section Formatting
 \newcommand{\ressection}[1]{%
-  \begin{center}
-    {\normalsize\bfseries\MakeUppercase{#1}}\\[4pt]
-    \hrule
-  \end{center}
-  \vspace{-4pt}%
+  \vspace{0.6em}
+  \noindent{\fontsize{11pt}{15.6pt}\selectfont\bfseries\MakeUppercase{#1}}\\[-8pt] 
+  \hrule height 1.35pt
+  \vspace{0.6em}
+}
+
+% Usage: \resumeEducation{University}{Faculty}{Graduation Date}
+        % {Bachelor}{GPA}
+\newcommand{\resumeEducation}[5]{
+  \noindent \textbf{#1}, #2 \hfill \textbf{#3} \\
+  #4 \hfill #5 \\
+}
+
+% Usage: \resumeExperience{Title}{Company}{Location}{Date}
+\newcommand{\resumeExperience}[4]{
+  \noindent \textbf{#1}, #2 - \textbf{#3} \hfill \textbf{#4}
+}
+
+% Usage: \resumeProject{Title}{Company/Location}{Date}
+\newcommand{\resumeProject}[3]{
+  \noindent \textbf{#1}, #2 \hfill \textbf{#3}
+  
 }
 
 \begin{document}
+\fontsize{11pt}{12.6pt}\selectfont 
 
 % Header
 \begin{center}
     {\LARGE \bfseries \Name} \\[4pt]
-    \scriptsize
-    \underline{\href{mailto:\Email}{\Email}} \,\textbar\, 
-    \Phone \,\textbar\, 
-    \underline{\href{\Linkedin}{linkedin.com/in/andrewobwocha}} \,\textbar\, 
-    \underline{\href{\Github}{github.com/AndrewObwocha}}
+    \small
+    \City \ | \ 
+    \href{mailto:\Email}{\Email} \ | \ 
+    \Phone \ | \ 
+    \href{\Linkedin}{linkedin.com/in/andrewobwocha} \ | \ 
+    \href{\Github}{github.com/AndrewObwocha}
 \end{center}
 
 % Education
-\small
 \ressection{Education}
-\textbf{The University of Alberta}, Edmonton AB \hfill May 2028 \\
-Bachelor of Science – Computer Science Honors \\
-\textbf{GPA:} 4.00/4.00 \,\textbar\, \textbf{SAT:} 1540/1600
 
-\vspace{-0.25em}
-
-\begin{itemize}[leftmargin=2.3em, label={}, itemsep=0pt, topsep=0pt, parsep=0pt]
-    \item \textbf{Coursework:} Multivariable Calculus, Linear Algebra, Probability, Data Structures, Machine Learning, Algorithms 
-    \item \textbf{Honours/Awards:}
-    
-    \vspace{-0.25em}
-
-    \begin{itemize}[leftmargin=2.3em, label={}, itemsep=0pt, topsep=0pt, parsep=0pt]
-        \item \textbf{Dean's List:} Currently inducted into the Dean's list at the Unviersity of Alberta for high performance
-        \item \textbf{Regional Excellence Scholar:} Recipient of CAD\$ 10,000 merit scholarship for East African Region
-        \item \textbf{UKMT Challenge Medalist:} 2x Silver medalist for Global British Mathematics Challenge in the Senior Division
-        \item \textbf{Valedictorian (1/256):} Recognized as highest performer in British A-Level Class of 2024 in Brookhouse School
-    \end{itemize}
-\end{itemize}
+\resumeEducation{University of Alberta}{Faculty of Science}{Expected June 2028}{Bachelor of Science in Computer Science; Minor in Economics}{Cumulative GPA: 4.00/4.00}
+\textit{Relevant Coursework: Programming Methodology, Databases, Algorithms, Data Structures, Discrete Math, Calculus}
 
 
-% Projects
-\ressection{Projects}
-
-\noindent \textbf{Knowledge Graph} \,\textbar\, Java, Javascript, Springboot, D3js
-\begin{itemize}[leftmargin=5em, itemsep=0pt, topsep=0pt, parsep=0pt]
-    \item Developed a graph-persistent web application using \textbf{React} and \textbf{D3.js} to empower researchers visualize and discover connections in information networks, improving on a cumbersome list-based interface
-    \item Refactored the backend from REST to \textbf{Springboot GraphQL}, resulting in a \textbf{5x reduction} in network trips.
-\end{itemize}
-
-\noindent \textbf{Tokenized Note System} \,\textbar\, Python, Javascript, Django, React
-\begin{itemize}[leftmargin=5em, itemsep=0pt, topsep=0pt, parsep=0pt]
-    \item Engineered a full-stack note system with \textbf{React} and \textbf{Django}, enabling \textbf{CRUD} operations on private material.
-    \item Reduced the user-click workflow for compiling notes by over 60\%, using \textbf{REST APIs} for centralized data access.
-\end{itemize}
-
-\noindent \textbf{House Forecaster} \,\textbar\, Java, Javascript, Springboot, D3js
-\begin{itemize}[leftmargin=5em, itemsep=0pt, topsep=0pt, parsep=0pt]
-    \item Engineered an ML pipeline using \textbf{Scikit-learn} in \textbf{Python}, incorporating data preprocessing, exploratory data analysis, and feature engineering to forecast housing prices in Manhattan, achieving an R² score of 0.78.
-    \item Engineered 2 novel features (HouseAge, NeighbourhoodSafety) that imprvoed R² score by 5 percentage points 
-\end{itemize}
 
 % Experience
 \ressection{Experience}
 
-\noindent \textbf{Kenya National Highways Authority}, Nairobi, KE \hfill June 2023 - August 2023 \\
-Software Engineering Intern \,\textbar\, Python, Django
-\begin{itemize}[leftmargin=5em, itemsep=0pt, topsep=0pt, parsep=0pt]
-    \item Implemented custom model validation using GeoDjango, eliminating 50+ monthly erroneous data points.
-    \item Automated system health checks with Python, achieving greater than 99\% uptime during 5-day financial closing.
+\resumeExperience{Software Engineering Intern}{Kenya National Highways Authority}{Nairobi, KE}{Jun 2024 - Aug 2024}
+\begin{itemize}[leftmargin=1.5em, noitemsep, topsep=0pt]
+    \item Eliminated 100+ silent request failures by enforcing geolocation boundaries in the Django serializer to return appropriate HTTP 500 error responses for invalid user coordinates.
+    \item Engineered an automated resource-monitoring daemon in Python using psutil, logging memory-leak events (80\%+ RAM usage) to improve workstation reliability.
+    \item Collaborated in an Agile team environment, utilizing Git/GitHub for version control and participating in peer code reviews.
+    \item Authored unit tests for the Django serializer and utilized Docker for local development and deployment.
+\end{itemize}
+
+% Projects
+\ressection{Projects}
+
+\resumeProject{\href{https://github.com/AndrewObwocha/ResumeEditor}{AI-Powered LaTeX Career Generator}}{Edmonton, AB}{Sep 2025 - Jan 2026}
+\begin{itemize}[leftmargin=1.5em, noitemsep, topsep=0pt]
+    \item Architected a FastAPI backend service in Python that generates a LaTeX resume script tailored to a job description.
+    \item Implemented a queue to handle high-latency service dependencies, including API calls to GitHub and the Gemini LLM. 
+    \item Integrated user-selected GitHub projects as LLM context by parsing README files from provided repository links.
+    \item Achieved 70\% test coverage in Pytest across project environment by developing a suite of unit and integration tests.
+\end{itemize}
+
+\vspace{5pt}
+
+\resumeProject{\href{https://github.com/AndrewObwocha/GraphMind}{Knowledge Graph Data API}}{Edmonton, AB}{Jun 2025 - Aug 2025}
+\begin{itemize}[leftmargin=1.5em, noitemsep, topsep=0pt]
+    \item Engineered a Spring Boot API microservice in Java to service a graph application through PostgreSQL queries to a relational database, providing node and relationship objects required to build a knowledge graph structure.
+    \item Enforced session security using JSON Web Tokens (JWT) with 30-minute automatic refreshes to avoid repetitive logins.
+    \item Implemented a GraphQL schema to avoid repetitive backend refactors of a rigid REST API from conflicting client needs.
+\end{itemize}
+
+\vspace{5pt}
+
+\resumeProject{\href{https://github.com/AndrewObwocha/UniNotes}{UniDoc API}}{Edmonton, AB}{Mar 2025 - May 2025}
+\begin{itemize}[leftmargin=1.5em, noitemsep, topsep=0pt]
+    \item Designed a Python Django RESTful API endpoint to service a course management application through SQLite queries to a relational database, providing proprietary course data.
+    \item Implemented a hierarchical design to eliminate redundant validation during GET requests, reducing latency by 60\%.
+\end{itemize}
+
+\vspace{5pt}
+
+\resumeProject{\href{https://github.com/AndrewObwocha/amazon-price-analyzer}{Amazon Price Statistics Extension}}{Edmonton, AB}{Nov 2025 - Dec 2025}
+\begin{itemize}[leftmargin=1.5em, noitemsep, topsep=0pt]
+    \item Developed a Chrome extension in Javascript that provides real-time statistical price analysis (quartiles, mean, std. deviation) across Amazon search results using client-side DOM parsing.
 \end{itemize}
 
 % Leadership
 \ressection{Leadership \& Community Involvement}
 
-\noindent \textbf{Undergraduate AI Society} \,\textbar\, Vice President of External Affairs \hfill March 2025 - Present
-\begin{itemize}[leftmargin=5em, itemsep=0pt, topsep=0pt, parsep=0pt]
-    \item Drove the 'Career Highlights' series, distilling complex career advice from senior leaders into actionable roadmaps for 100+ students, bridging the industry-academia gap and platforming students to network with professionals.
-    \item Recruited a diverse panel (PhD, MSc, Prof) for comparative look at academic vs. industry R\&D, demystifying graduate pathways for undergraduates, with over 75 students in attendance for the moderated session.
-\end{itemize}
-    
-\noindent \textbf{Univeristy of Alberta Innovation Fund} \,\textbar\, AI Analyst \hfill August 2025 - Present
-\begin{itemize}[leftmargin=5em, itemsep=0pt, topsep=0pt, parsep=0pt]
-    \item Applied risk assessment frameworks to evaluate the architectural designs of 2 AI startups, informing investors.
-    \item Spearheaded standardization of assessment protocols by training a 5-person team on startup declarative analysis.
+\resumeExperience{Vice President of External Affairs}{Undergraduate AI Society (800 members)}{Edmonton, AB}{Mar 2025 - Present}
+\begin{itemize}[leftmargin=1.5em, noitemsep, topsep=0pt]
+    \item Hosted 12 interviews with professional Software Engineers discussing AI's impact on the software development life cycle (SDLC), reaching 1000 students and addressing career anxiety in the club community.
+    \item Organized R\&D career panel of 4 research academics (Prof/PhD/Masters/Undergraduate) to clarify the early steps to starting reputable CS research, gathering 100+ undergraduate attendees.
 \end{itemize}
 
 % Skills
 \ressection{Technical Skills}
-\textbf{Languages \& Libraries:}  Python, JavaScript, SQL, HTML, CSS, React, Django, REST APIs, NumPy \\
-\textbf{Tools \& Platforms:} Git, VSCode, Jupyter
+\noindent \textbf{Languages:} Python, C, Java, SQL, JavaScript, LaTeX, Bash \\
+\textbf{Frameworks:} Django, SpringBoot, Gradle, RESTful APIs, GraphQL, Pytest, React \\
+\textbf{Tools:} Git, GitHub, Linux, PostgreSQL, Docker, VSCode
 
 \end{document}
 """
